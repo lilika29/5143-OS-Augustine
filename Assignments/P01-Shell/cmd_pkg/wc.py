51d01a6d675ed1e0fce88c858ae8f89ab44836ad
1063-DS-Kalantari <l.aghdam@gmail.com>
2021-02-09 18:27:23 -0600

    Update wc.py

--- Assignments/P01-Shell/cmd_pkg/wc.py
+++ Assignments/P01-Shell/cmd_pkg/wc.py
@@ -1,66 +1,113 @@
-#!usr/bin/env python 3
-import re # used to get number of words
+'''Dr griffin
+code
+'''
+'''
+Name
+     wc
+     
+Discription 
+            wc filename    #counts num of lines words charecters 
+            wc -l filename #counts num of lines 
+            wc -m filename #counts num of words
+            wc -w filename #counts num of char 
 
-def wc(flags,params,directs):
-  # If no flags were passed in, do all three
-  if not flags:
-    filenum = 0
-    for p in params:
-      with open(params[filenum], "r") as f:
-        lines = f.read().splitlines()
-      
-      with open(params[filenum], "r") as f:
-        words = f.read()
+'''
 
-      numlines = len(lines)
-      result = len(re.findall(r'\w+', words))
-      chars = len(words)
 
-      print("There are ", numlines, " lines in", params[filenum])
-      print("There are ", result," words in", params[filenum])
-      print("There are ", chars, "characters in the file", params[filenum])
-      filenum += 1
+import re  # Regular Expression(RE) Syntax AND
+import sys
+from cmd_pkgs.arg_parser import ArgParse
+from cmd_pkgs.return_status import ReturnStatus
 
-  # If flags were passed in, do the requested action.
-  else:
-    # Get the number of lines in the file
-    filenum = 0
-    if flags[0] == "-l":
-      for p in params:
-        with open(params[filenum], "r") as f:
-          lines = f.read().splitlines()
+# "re" module included with Python primarily used for string searching and manipulation
+## And it is necessory for using regex (findall()) function
+from cmd_pkgs.return_status import ReturnStatus
 
-        numlines = len(lines)
+arg_dict = {
+    'l': False,
+    'm': False,
+    'w': False,
+}
 
-        print("There are ", numlines, " lines in ", params[filenum])
-        filenum += 1
 
-    # Find the number of words present in the file. (Special characters are generally ignored.)
-    elif flags[0] == "-w":
-      filenum = 0
-      for p in params:
-        with open(params[filenum], "r") as f:
-          words = f.read()
+def wc(args, cwd):
+    arg_parse = ArgParse(args, arg_dict, cwd, __doc__)
+    flags = arg_parse.get_flags()
+    rs = ReturnStatus()
+    rs.set_cwd(cwd)
+    directories = arg_parse.get_directories()
 
-        result = len(re.findall(r'\w+', words))
+    rs.set_return_values("\n")
+    rs.set_return_values("hello world1")
+    sys.stdout.flush()
+    if not flags:
+        rs.set_return_values("helloooo world")
+        filenum = 0
 
-        print("There are ", result," words in", params[filenum])
-        filenum += 1
+        for p in directories:
+            with open(directories[filenum], "r") as f:
+                lines = f.readlines()  # Use f.read() combined with splitlines()The splitlines()
+                # method splits a string into a list. The splitting is done at line breaks.
 
-    elif flags[0] == "-m":
-      filenum = 0
-      for p in params:
-        with open(params[filenum], "r") as f:
-          chars = f.read()
+            with open(directories[filenum], "r") as f:
+                words = f.read()
 
-        result = len(chars)
+            numlines = str(len(lines))
+            rs.set_return_values(numlines + " ")  # prints number of lines
+            
+            numwords = str(len(words.split())) + " "  #  count the
+            # number of words in the sentence available
+            # in a regex module.
+            rs.set_return_values(numwords)
 
-        print("There are ", result, "characters in the file ", params[filenum])
-        filenum += 1
+            chars = str(len(words))
+            rs.set_return_values(chars + " ")
+
+            sys.stdout.flush()
+
+            filenum += 1
+
 
     else:
-      print("Not a valid flag value. The flags are -l, -m, and -w.")
+
+        # wc-l is getting the number of lines in the file
+        filenum = 0
+        if flags[0] == "-l":
+            for p in directories:
+                with open(directories[filenum], "r") as f:
+                    lines = f.read().splitlines()
+
+                    lines = len(lines)
+
+                rs.set_return_values(lines, " lines ")
+                filenum += 1
+
+        # Find the number of words wc -w
+        elif flags[0] == "-w":
+            filenum = 0
+            for p in directories:
+                with open(directories[filenum], "r") as f:
+                    words = f.read()
+
+                numwords = len(re.findall(r'\w+', words))
+
+                rs.set_return_values(numwords, " words")
+
+                filenum += 1
+        # wc -m finding number of charecters
+        elif flags[0] == "-m":
+            filenum = 0
+            for p in directories:
+                with open(directories[filenum], "r") as f:
+                    chars = f.read()
+
+                numchar = len(chars)
+
+                rs.set_return_values(numchar, "characters")
+                filenum += 1
+
+        else:
+            rs.set_return_values("Invalid")
+    return rs
 
 
-if __name__ == "__main__":
-  wc(["-c"],["five.txt"]," ")
